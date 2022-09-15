c802f2f7702af236b7f3798cdb3269cc93fa2035
Jarrian <glorianagojar@gmail.com>
2022-09-15 16:26:37 +0800

    update

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const inputField = document.querySelectorAll('.input-field');
 const inputForm = document.querySelector('.input-form');
 
-const isValidFirstName = false;
-const isValidLastName = false;
-const isValidEmailAddress = false;
-const isValidPassword = false;
+var isValidFirstName;
+var isValidLastName;
+var isValidEmailAddress;
+var isValidPassword;
+var isValidPasswordLength;
 
 inputField.forEach(i => i.addEventListener('input', (e) => {
   const group = e.target.parentNode;
   const field = group.querySelector('.input-field');
   const invalid = group.querySelector('.input-invalid');
+  
+  const input = field.value;
+  const inputType = field.placeholder;
+  const inputPattern = field.dataset.pattern;
+  
+  if(!validateInput(input, inputType)){
+    if (inputType.toLowerCase() == 'email address') invalid.textContent = `${inputType} should follow this pattern ${inputPattern}`;
+    else invalid.textContent = `${inputType} should only contain characters ${inputPattern}`;
+    invalid.classList.add('show-invalid');
+  } else {
+    invalid.textContent = '';
+    invalid.classList.remove('show-invalid');
+  }
+
+  console.log(inputType);
 }));
 
 inputForm.addEventListener('submit', (e) => {
@@ -30,3 +46,26 @@ inputForm.addEventListener('submit', (e) => {
     }
   });
 });
+
+function validateInput(input, type) {
+  if (type.toLowerCase() == 'first name'){
+    isValidFirstName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/.test(input);
+    return isValidFirstName;
+  }
+
+  if (type.toLowerCase() == 'last name'){
+    isValidLastName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/.test(input);
+    return isValidLastName;
+  }
+
+  if (type.toLowerCase() == 'email address'){
+    isValidEmailAddress = input.toLowerCase()
+                                .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    return isValidEmailAddress;
+  }
+
+  if (type.toLowerCase() == 'password'){
+    isValidPassword = input.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/);
+    return isValidPassword;
+  }
+}
