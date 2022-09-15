96353dfe8e7e825540953996433f1c5eb56cd147
Jarrian <glorianagojar@gmail.com>
2022-09-15 14:50:49 +0800

    update

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const inputField = document.querySelectorAll('.input-field');
+const inputForm = document.querySelector('.input-form');
+
+const isValidFirstName = false;
+const isValidLastName = false;
+const isValidEmailAddress = false;
+const isValidPassword = false;
 
 inputField.forEach(i => i.addEventListener('input', (e) => {
   const group = e.target.parentNode;
   const field = group.querySelector('.input-field');
   const invalid = group.querySelector('.input-invalid');
-  invalid.textContent = field.value;
 }));
+
+inputForm.addEventListener('submit', (e) => {
+  console.log(e.target);
+  e.preventDefault();
+});
